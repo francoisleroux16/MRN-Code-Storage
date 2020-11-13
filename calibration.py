d8d0a7e1cd6cabfb08adebf302e327f98f9134be
Francois <55328931+francoisleroux16@users.noreply.github.com>
2020-11-13 14:29:24 +0200

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -3,7 +3,7 @@
 Created on Tue Sep 22 18:32:01 2020
 
 @author: Francois le Roux
-This python script is to be used for all processing to be done regarding calibration of the five-hole probe.
+- This python script is to be used for all processing to be done regarding calibration of the five-hole probe.
 """
 import importing
 import numpy as np
